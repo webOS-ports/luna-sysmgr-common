a15cfe3401e654380221fd98608795e5144b87bf
Herrie <github.com@herrie.org>
2016-02-04 19:42:54 +0100

    Fix lifecycle of json string

--- include/JSONUtils.h
+++ include/JSONUtils.h
@@ -220,13 +220,16 @@ jvalue_ref convert_schema_v2_to_v4(const char *schema_v2);
 #define VALIDATE_SCHEMA_AND_RETURN_OPTION(lsHandle, message, schema, schErrOption) {\
                                                                                         jvalue_ref schema_v4 = convert_schema_v2_to_v4(schema);                                                 \
                                                                                         LSMessageJsonParser jsonParser(message, jvalue_tostring_simple(schema_v4));                             \
-                                                                                        if (!jis_null(schema_v4))                                                                               \
-                                                                                            j_release(&schema_v4);                                                                              \
                                                                                                                                                                                                 \
                                                                                         if (EDefault == schErrOption)                                                                           \
                                                                                             schErrOption = static_cast<ESchemaErrorOptions>(Settings::LunaSettings()->schemaValidationOption);  \
                                                                                                                                                                                                 \
-                                                                                        if (!jsonParser.parse(__FUNCTION__, lsHandle, schErrOption))                                            \
+                                                                                        int res = jsonParser.parse(__FUNCTION__, lsHandle, schErrOption);                                       \
+											                                                                                                        \
+                                                                                        if (!jis_null(schema_v4))                                                                               \
+                                                                                            j_release(&schema_v4);                                                                              \
+											                                                                                                        \
+                                                                                        if (!res)                                                                                               \
                                                                                             return true;                                                                                        \
                                                                                     }
 
